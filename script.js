343077e407f05fa4f505f947145eb460c10626a2
chamokBarmon <chamokbarmon123@gmail.com>
2022-09-15 01:15:09 +0600

    four

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const display = document.getElementById("display");
-const question = document.getElementById("question");
-const startBtn = document.getElementById("starts");
-const countdownOverlay = document.getElementById("countdown");
-const resultModal = document.getElementById("result");
-const modalBackground = document.getElementById("modal-background");
+const display = document.getElementById('display');
+const question = document.getElementById('question');
+const startBtn = document.getElementById('starts');
+const countdownOverlay = document.getElementById('countdown');
+const resultModal = document.getElementById('result');
+const modalBackground = document.getElementById('modal-background');
 
 // variables
 let userText = '';
@@ -12,7 +12,7 @@ let startTime;
 let questionText = '';
 
 // Load and display question
-fetch("./texts.json")
+fetch('./texts.json')
   .then((res) => res.json())
   .then((data) => {
     questionText = data[Math.floor(Math.random() * data.length)];
@@ -24,14 +24,14 @@ const typeController = (e) => {
   const newLetter = e.key;
 
   // Handle backspace press
-  if (newLetter == "Backspace") {
+  if (newLetter == 'Backspace') {
     userText = userText.slice(0, userText.length - 1);
     return display.removeChild(display.lastChild);
   }
 
   // these are the valid character we are allowing to type
   const validLetters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890!@#$%^&*()_+-={}[]'\".,?";
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890!@#$%^&*()_+-={}[]\'".,?';
 
   // if it is not a valid character like Control/Alt then skip displaying anything
   if (!validLetters.includes(newLetter)) {
@@ -43,9 +43,14 @@ const typeController = (e) => {
   const newLetterCorrect = validate(newLetter);
 
   if (newLetterCorrect) {
-    display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
+    display.innerHTML += `<span class="green">${
+      newLetter === ' ' ? '▪' : newLetter
+    }</span>`;
   } else {
-    display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount++;
+    display.innerHTML += `<span class="red">${
+      newLetter === ' ' ? '▪' : newLetter
+    }</span>`;
   }
 
   // check if given question text is equal to user typed text
@@ -63,20 +68,20 @@ const validate = (key) => {
 
 // FINISHED TYPING
 const gameOver = () => {
-  document.removeEventListener("keydown", typeController);
+  document.removeEventListener('keydown', typeController);
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = Math.floor((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = '';
-  resultModal.classList.remove("hidden");
-  modalBackground.classList.remove("hidden");
+  resultModal.classList.remove('hidden');
+  modalBackground.classList.remove('hidden');
   // clear user text
   display.innerHTML = '';
   // make it inactive
-  display.classList.add("inactive");
+  display.classList.add('inactive');
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
@@ -91,12 +96,12 @@ const gameOver = () => {
   startTime = null;
   errorCount = 0;
   userText = '';
-  display.classList.add("inactive");
+  display.classList.add('inactive');
 };
 
 const closeModal = () => {
-  modalBackground.classList.toggle("hidden");
-  resultModal.classList.toggle("hidden");
+  modalBackground.classList.toggle('hidden');
+  resultModal.classList.toggle('hidden');
 };
 
 const start = () => {
@@ -132,8 +137,9 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent =  Math.floor(currentTime - startTime) / 1000;
+  const timeSpent = Math.floor((currentTime - startTime) / 1000);
 
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById('show-time').innerHTML = `${
+    startTime ? timeSpent : 0
+  } seconds`;
 }, 1000);
